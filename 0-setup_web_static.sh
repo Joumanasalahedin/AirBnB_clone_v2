2fb5d1b748a94414c4f9374b257e65b589d6c2ee
Joumana <joumanasalahedin@gmail.com>
2023-12-08 18:15:58 +0100

    task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,6 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
-    <title>Holberton School</title>
   </head>
   <body>
     Holberton School
@@ -25,6 +24,6 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/server {/a \\tlocation \/hbnb_static\/ { alias \/data\/web_static\/current\/; index index.html; }' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
